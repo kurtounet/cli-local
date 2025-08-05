e0252036c964567638488bbe6c66e5fc61eb7476
kurtounet <bossut.anthony@gmail.com>
2025-08-05 18:34:49 +0200

    feat(tools): Add tree analysis tools

---
+++ my-cli/src/features/tools/tree/services/generate-tree-json.service.ts
@@ -0,0 +1,97 @@
+import path from "path";
+import fs from "fs";
+import { verifyInGitIgnoreFile } from "@features/frameworks/_global/service/git.service";
+
+const DEFAULT_IGNORED_FOLDERS = [
+  ".angular",
+  ".qodo",
+  ".vscode",
+  "dist",
+  "node_modules",
+  ".git",
+  "build",
+  "var",
+  "vendor",
+];
+type TreeNode = {
+  _type: "file" | "directory";
+  name: string;
+  gitIgnore: boolean;
+  pathInProject: string;
+  createdAt: Date;
+  updatedAt: Date;
+  children?: TreeNode[];
+};
+
+export function generateTreeJson(
+  dir: string,
+  maxDepth: number,
+  currentDepth: number = 0,
+  ignoredFolders: string[] = DEFAULT_IGNORED_FOLDERS,
+): TreeNode | null {
+  if (currentDepth > maxDepth) return null;
+
+  try {
+    const stats = fs.statSync(dir);
+    const name = path.basename(dir);
+
+    if (!stats.isDirectory()) {
+      return {
+        _type: "file",
+        name,
+        gitIgnore: verifyInGitIgnoreFile(process.cwd() + "/.gitignore", dir),
+        pathInProject: dir,
+        createdAt: stats.birthtime,
+        updatedAt: stats.mtime,
+      };
+    }
+
+    const children: TreeNode[] = [];
+
+    const items = fs.readdirSync(dir, { withFileTypes: true });
+
+    for (const item of items) {
+      if (ignoredFolders.includes(item.name)) continue;
+
+      const fullPath = path.join(dir, item.name);
+
+      if (item.isDirectory()) {
+        const child = generateTreeJson(
+          fullPath,
+          maxDepth,
+          currentDepth + 1,
+          ignoredFolders,
+        );
+        if (child) children.push(child);
+      } else {
+        children.push({
+          _type: "file",
+          name: item.name,
+          gitIgnore: verifyInGitIgnoreFile(process.cwd() + "/.gitignore", dir),
+          pathInProject: dir,
+          createdAt: stats.birthtime,
+          updatedAt: stats.mtime,
+        });
+      }
+    }
+
+    return {
+      _type: "directory",
+      name,
+      gitIgnore: verifyInGitIgnoreFile(process.cwd() + "/.gitignore", dir),
+      pathInProject: dir,
+      createdAt: stats.birthtime,
+      updatedAt: stats.mtime,
+      children,
+    };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(
+        `❌ Erreur lors de la lecture du dossier : ${error.message}`,
+      );
+    } else {
+      console.error("❌ Une erreur inconnue est survenue.");
+    }
+    return null;
+  }
+}

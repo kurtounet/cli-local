8073f66aa1383c054353674065b4093e8ef5be5e
kurtounet <bossut.anthony@gmail.com>
2026-01-24 23:37:24 +0100

    add getDirectoryTree

--- src/types/file-system.interface.ts
+++ src/types/file-system.interface.ts
@@ -1,9 +1,9 @@
-// src/types/file-system.interface.ts
 export interface IFileSystemService {
   exists(targetPath: string): Promise<boolean>;
   createDirectory(dirPath: string): Promise<void>;
   writeFile(filePath: string, content: string): Promise<void>;
   readFile(filePath: string): Promise<string>;
   copy(source: string, destination: string): Promise<void>;
+  getDirectoryTree(dirPath: string): any;
   resolvePath(...segments: string[]): string;
 }

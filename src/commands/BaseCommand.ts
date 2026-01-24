66cafa8f5776c936b5d662e0cf76f8ca52b24da9
kurtounet <bossut.anthony@gmail.com>
2026-01-24 22:41:50 +0100

    add jsDoc

--- src/commands/BaseCommand.ts
+++ src/commands/BaseCommand.ts
@@ -1,7 +1,21 @@
 import { IAppContext } from "../types/context.interface.js";
 import { ILoggerService } from "@/types/logger-service.interface.js";
 import { ICommand, ICommandOption } from "@/types/command.interface.js";
+/**
+ * @typedef {Object} CommandOption
+ * @property {string} flags - Le format de l'option (ex: "-f, --force")
+ * @property {string} description - Explication de l'option
+ * @property {any} [defaultValue] - Valeur par défaut
+ */
 
+/**
+ * Classe abstraite de base pour les commandes
+ * * @property {string} name - Nom de la commande
+ * @property {string} description - Description de la commande
+ * @property {CommandOption[]} options - Liste des options disponibles
+ * @property {string} arguments - Syntaxe des arguments (ex: "<type> [names...]")
+ * @property {string[]} aliases - Raccourcis de la commande
+ */
 export abstract class BaseCommand implements ICommand {
   // Propriétés obligatoires pour chaque commande
   public abstract name: string;
@@ -24,10 +38,7 @@ export abstract class BaseCommand implements ICommand {
   /**
    * Méthode principale à implémenter dans chaque commande
    */
-  public abstract execute(
-    args: string[],
-    options: Record<string, unknown>,
-  ): Promise<void>;
+  public abstract execute(args: string[], options: Record<string, unknown>): Promise<void>;
 
   /**
    * Utilitaire pour récupérer un service rapidement

356755f35403dbcfbe6ad55e0c3a0b18a4c96435
kurtounet <bossut.anthony@gmail.com>
2026-02-08 16:28:35 +0100

    remove

--- src/services/task-manager.service.ts
+++
@@ -1,7 +0,0 @@
-import { ITaskManagerService } from "@/types/task-manager-service.interface.js";
-
-import { BaseService } from "./base-service.service.js";
-
-export class TaskManagerService
-  extends BaseService
-  implements ITaskManagerService {}

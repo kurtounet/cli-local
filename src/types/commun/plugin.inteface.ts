c93ea93de91ae7ab8750fd5bf6f13b73df4a9e57
kurtounet <bossut.anthony@gmail.com>
2026-02-10 09:38:56 +0100

    delete

--- src/types/commun/plugin.inteface.ts
+++
@@ -1,8 +0,0 @@
-import { IPluginManifest } from "../plugin.interface.js";
-import { IBagData } from "./data-bag.interface.js";
-
-export interface IPluginData {
-  manifest: IPluginManifest;
-  pluginDir: string;
-  data: IBagData;
-}

930271e31d974ab2c28e52bc11395021d2adcb41
kurtounet <bossut.anthony@gmail.com>
2026-02-10 22:24:49 +0100

    feat: update project feature service

--- src/features/project/services/project.service.ts
+++ src/features/project/services/project.service.ts
@@ -84,7 +84,9 @@ export class ProjectService implements IProjectService {
       this.cli.logger.info(`Traitement du framework : ${framework.name}`);
       // 1. On récupère le service spécifique au framework
       const specificService = this.selector.getService(framework.name);
-      this.cli.logger.info(`${EMOJI.rond_green} Génération des fichiers de base...`);
+      this.cli.logger.info(
+        `${EMOJI.rond_green} Génération des fichiers de base...`,
+      );
       await specificService.generate(configProject, entitiesJson, fileMdj);
     }
     return "Projet généré avec succès !";
@@ -125,8 +127,12 @@ export class ProjectService implements IProjectService {
     const absolutePath = path.resolve(targetPath);
 
     // 1. Détection des fichiers importants
-    const hasConfig = this.cli.fileSystem.exists(this.cli.path.join(absolutePath, ".mclprc.json"));
-    const hasCliLocal = this.cli.fileSystem.exists(this.cli.path.join(absolutePath, ".cli-local"));
+    const hasConfig = this.cli.fileSystem.exists(
+      this.cli.path.join(absolutePath, ".mclprc.json"),
+    );
+    const hasCliLocal = this.cli.fileSystem.exists(
+      this.cli.path.join(absolutePath, ".cli-local"),
+    );
 
     // On récupère tous les fichiers pour checker le StarUML
     const allFiles = await this.cli.fileSystem.readDir(absolutePath);
@@ -150,7 +156,12 @@ export class ProjectService implements IProjectService {
     // ACTION : Sécurité .cli-local
     if (!hasCliLocal) {
       this.cli.logger.warn(`⚠️ Dossier .cli-local manquant, initialisation...`);
-      this.cli.shell.executeSyncSpawn("mclp", ["p", "new", absolutePath], "inherit", true);
+      this.cli.shell.executeSyncSpawn(
+        "mclp",
+        ["p", "new", absolutePath],
+        "inherit",
+        true,
+      );
     } else {
       this.cli.logger.success(`✅ Projet conforme.`);
     }
@@ -161,22 +172,34 @@ export class ProjectService implements IProjectService {
   private async initProject(path: string): Promise<void> {
     const allDirAndFiles: string[] = await this.cli.fileSystem.readDir(path);
 
-    this.cli.logger.info(`${EMOJI.help} Vérification de l'existance du dossier: .cli-local`);
+    this.cli.logger.info(
+      `${EMOJI.help} Vérification de l'existance du dossier: .cli-local`,
+    );
     if (!allDirAndFiles.includes(".cli-local")) {
-      this.cli.logger.warn(`${EMOJI.warning} Le dossier .cli-local n'existe pas`);
+      this.cli.logger.warn(
+        `${EMOJI.warning} Le dossier .cli-local n'existe pas`,
+      );
       //TODO créer le dossier .cli-local
     } else {
-      this.cli.logger.success(`${EMOJI.success} Le dossier .cli-local existe déjas`);
+      this.cli.logger.success(
+        `${EMOJI.success} Le dossier .cli-local existe déjas`,
+      );
       //TODO Récupérer les fichiers du dossier .cli-local
       //
     }
 
-    this.cli.logger.info(`${EMOJI.help} Vérification de l'existance du fichier: .mclprc.json`);
+    this.cli.logger.info(
+      `${EMOJI.help} Vérification de l'existance du fichier: .mclprc.json`,
+    );
     if (!allDirAndFiles.includes(".mclprc.json")) {
-      this.cli.logger.warn(`${EMOJI.warning} Le fichier .mclprc.json existe pas`);
+      this.cli.logger.warn(
+        `${EMOJI.warning} Le fichier .mclprc.json existe pas`,
+      );
       //TODO créer le fichier .mclprc.json
     } else {
-      this.cli.logger.success(`${EMOJI.success} Le fichier .mclprc.json existe déjas`);
+      this.cli.logger.success(
+        `${EMOJI.success} Le fichier .mclprc.json existe déjas`,
+      );
       //TODO Mettre le fichier .mclprc.json à jour,
       // avec les des data des fichiers de .cli-local
     }
